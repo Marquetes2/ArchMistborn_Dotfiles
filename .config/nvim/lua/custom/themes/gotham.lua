b85cbbc25ce28d5483703433c6befa9dd25e13a8
Marquetes2 <marquetes2@gmail.com>
2023-09-13 19:23:15 +0200

    added themes to nvim

---
+++ .config/nvim/lua/custom/themes/gotham.lua
@@ -0,0 +1,57 @@
+local M = {}
+
+M.base_30 = {
+  white = "#98d1ce",                      -- #c7ccd1
+  darker_black = "#081f2d",               -- #161a1d
+  black = "#0e2937",                      -- #2c2023 nvim bg
+  black2 = "#093748",                     -- #24282b
+  one_bg = "#195465",                     -- #272b2e
+  one_bg2 = "#195465",                    -- #303437
+  one_bg3 = "#195465",                    -- #3a3e41
+  grey = "#195465",                       -- #44484b
+  grey_fg = "#4e5165",                    -- #4a4e51
+  grey_fg2 = "#195465",                   -- #515558
+  light_grey = "#33859d",                 -- #565a5d
+  red = "#c33027",                        -- #c79595
+  baby_pink = "#888ba5",                  -- #d09eb7
+  pink = "#888ba5",                       -- #c795ae
+  line = "#195465",                       -- #303437
+  green = "#26a98b",                      -- #aec795
+  vibrant_green = "#26a98b",              -- #95c7ae
+  nord_blue = "#599caa",                  -- #8ca5be
+  blue = "#195465",                       -- #95aec7
+  yellow = "#edb54b",                     -- #c7c795
+  sun = "#edb54b",                        -- #d0d09e
+  purple = "#4e5165",                     -- #ae95c7
+  dark_purple = "#4e5165",                -- #a58cbe
+  teal = "#599caa",                       -- #8fb4b5
+  orange = "#d26939",                     -- #c7ae95
+  cyan = "#33859d",                       -- #9eb7d0
+  statusline_bg = "#133442",              -- #23272a
+  lightbg = "#093748",                    -- #303437
+  pmenu_bg = "#26a98b",                   -- #99c366
+  folder_bg = "#599caa",                  -- #8ca5be
+}
+
+M.base_16 = {
+  base00 = "#0a0f14", -- #1c2023
+  base01 = "#081f2d", -- #272b2e
+  base02 = "#093748", -- #303437
+  base03 = "#195465", -- #44484b
+  base04 = "#d3ebe9", -- #adb3ba
+  base05 = "#c4e4e2", -- #c7ccd1
+  base06 = "#addad7", -- #dfe2e5
+  base07 = "#98d1ce", -- #f3f4f5
+  base08 = "#33859d", -- #c7ae95
+  base09 = "#33859d", -- #c7c795
+  base0A = "#599caa", -- #aec795
+  base0B = "#888ba5", -- #95c7ae
+  base0C = "#edb54b", -- #95aec7
+  base0D = "#c33027", -- #ae95c7 
+  base0E = "#26a98b", -- #c795ae 
+  base0F = "#98d1ce", -- #c79595
+}
+
+M.type = "dark"
+
+return M
